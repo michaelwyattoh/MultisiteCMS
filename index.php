574e8d70337581fa7f2b49b24a868f78214d45a0
Michael Wyatt <michael@michaelwyatt.net>
2013-12-19 20:54:02 -0500

    add navigation

--- index.php
+++ index.php
@@ -1,1 +1,50 @@
-//PHP Content
+<?
+
+require_once 'configure.php';
+$thisPage = $_SERVER["REQUEST_URI"];
+require_once 'getContent.php';
+require("$DOCUMENT_ROOT/getNavArray.php");
+
+
+//TIME
+function niceDate($d) {
+	$dArr = explode("-",$d);
+	$dYear =  $dArr[0];
+	$dMonth =  $dArr[1];
+	$dDay =  $dArr[2];
+	
+	$timestamp = mktime(0,0,0,$dMonth,$dDay,$dYear);
+	
+	return date("M j, Y",$timestamp);
+}
+
+//END TIME
+?>
+ <body> 
+ 
+    	<div id='wrapper'>
+    	<header><?=$siteTitle?></header>
+    	<hr>
+    	<nav>
+        <?php 
+							foreach($navigationArray as $n) {
+							$title = $n['contentTitle'];
+							$page = $n['contentPage'];
+							$id = $n['contentID'];
+							$link = $n['contentLink'];
+							
+							if($page==$thisPage)
+								$activeClass="active";
+							else
+								$activeClass="";
+
+							echo "<li><a class='$activeClass' href='$page' title='$title'>$title</a></li>";
+						}
+        ?>
+        </nav>
+<?php
+        
+	   if ($thisPage == "/$subDirectory$page" || $thisPage =="$page"){
+	   
+	   
+	   }
